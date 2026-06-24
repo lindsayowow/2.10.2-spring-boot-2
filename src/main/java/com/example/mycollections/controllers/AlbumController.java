b08334cb920a25068dcfa7208cdc3e98970c6d3e
lindsayowow <lindsayow@gmail.com>
2026-06-24 14:15:05 -0500

    added code for Album

---
+++ src/main/java/com/example/mycollections/controllers/AlbumController.java
@@ -0,0 +1,45 @@
+package com.example.mycollections.controllers;
+
+import com.example.mycollections.models.Album;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/albums")
+public class AlbumController {
+
+    private final List<Album> albums = new ArrayList<>() {{
+        add(new Album("Love Symbol", "Prince", 1992, 18));
+        add(new Album("Dookie", "Green Day", 1994, 14));
+        add(new Album("Pies Descalzos", "Shakira", 1995, 11));
+    }};
+
+    @GetMapping("/json")
+    public List<Album> getAlbumsJson() {
+        return albums;
+    }
+
+    @GetMapping("/html")
+    public String getAlbumsHtml() {
+        String albumList = "<ul>";
+        for (Album album : albums) {
+            albumList += "<li>" + album + "</li>";
+        }
+        albumList += "</ul>";
+
+        return """
+                <html>
+                    <body>
+                        <h1>Albums</h1>
+                        <ul>
+                """ +
+                albumList +
+                """
+                        </ul>
+                    </body>
+                """;
+    }
+
+}
